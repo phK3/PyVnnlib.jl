fbc3f7ba26feea034c69ebf6c6e6ef9f0523bf48
phK3 <ufedz@student.kit.edu>
2023-06-30 16:43:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Simple Julia interface to the vnnlib parser by dlshriver (see https://github.com/dlshriver/vnnlib).
+
+# Installation
+
+Type `]` in the Julia REPL to activate the package manager.
+
+```
+add https://github.com/phK3/...
+```
+
+Make sure the python `vnnlib` parser is available in the python environment used by `PyCall.jl`.
---
+++ src/PyVnnlib.jl
@@ -0,0 +1,96 @@
+module PyVnnlib
+
+using PyCall, JLD2
+
+const vnnlib = PyNull()
+const compat = PyNull()
+
+function __init__()
+    pushfirst!(PyVector(pyimport("sys")["path"]), @__DIR__)
+    copy!(vnnlib, pyimport("vnnlib"))
+    # why can't i just use vnnlib.compat, but have to specifically pyimport that submodule?
+    copy!(compat, pyimport("vnnlib.compat"))
+end
+
+
+"""
+Generate matrix of properties given input file.
+
+The output is a matrix of shape n × 2 with one row for each of the n properties.
+Each row is a vector [bounds, output_specs]
+"""
+function parse_properties(infile; verbosity=0)
+    verbosity == 1 && println("parsing file $infile") 
+    t_parse = @elapsed parsed = vnnlib.parse_file(infile, strict=false)
+
+    verbosity == 1 && println("extracting properties")
+    t_transform = @elapsed properties = compat.CompatTransformer("X","Y").transform(parsed)
+
+    verbosity == 1 && println("parsing: $t_parse, transforming: $t_transform")
+    return properties
+end
+
+
+"""
+Generate list of specifications [(l, u, [(A, b)])] of
+- input bounds l, u for each input 
+- polytopes Ax ≤ b over each common input space (s.t. property is SAT, if one of the polytopes is SAT)
+"""
+function get_speclist(props)
+    specs = []
+    for i in axes(props,1)
+        bounds = props[i,1]
+        lbs = bounds[:,1]
+        ubs = bounds[:,2]
+
+        output_specs = props[i,2]
+
+        # list of (A, b) s.t. Ax ≤ b
+        out_specs = [(o[1], o[2]) for o in output_specs]
+        push!(specs, (lbs, ubs, out_specs))
+    end
+
+    return specs
+end
+
+
+"""
+Generate list of specifications [(l, u, [(A, b)])] from a vnnlib file of
+- input bounds l, u for each input 
+- polytopes A₁x ≤ b₁ ∨ A₂x ≤ b₂ ∨ ... over each common input space
+"""
+function generate_specs(vnnlib_file; verbosity=0)
+    props = parse_properties(vnnlib_file, verbosity=verbosity)
+    speclist = get_speclist(props)
+
+    return speclist
+end
+
+
+"""
+Generates a list of specifications and saves it as outfile.
+
+The list of specifications has format [(l, u, [(A, b)])] from a vnnlib file of
+- input bounds l, u for each input 
+- polytopes A₁x ≤ b₁ ∨ A₂x ≤ b₂ ∨ ... over each common input space
+
+The list of specifications can be loaded from the outfile by calling
+
+speclist = load(outfile, "speclist")
+
+args:
+    vnnlib_file - either a .vnnlib or a .vnnlib.gz file
+    outfile - name of output file has to end in .jld2
+"""
+function save_specs(vnnlib_file, outfile; verbosity=0)
+    speclist = generate_specs(vnnlib_file, verbosity=verbosity)
+
+    save(outfile, "speclist", speclist)
+end
+
+
+export generate_specs, save_specs
+
+
+
+end # module PyVnnlib
